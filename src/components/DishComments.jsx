5f442ca9cb67c5c1abcccabc354f5a545b93da05
bioback <78535998+bioback@users.noreply.github.com>
2021-03-30 14:04:20 +0200

    class dishcomments

--- src/components/DishComments.jsx
+++ src/components/DishComments.jsx
@@ -1,18 +1,35 @@
-import { ListGroup } from 'react-bootstrap'
+import { ListGroup } from "react-bootstrap";
+import React from "react";
 
-const DishComments = (props) =>
-    props.selectedDish
-        ? <ListGroup>
-            <h2>Comments for {props.selectedDish.name}</h2>
-            {
-                props.selectedDish.comments.map(c => (
+// const DishComments = (props) =>
+//     props.selectedDish
+//         ? <ListGroup>
+//             <h2>Comments for {props.selectedDish.name}</h2>
+//             {
+//                 props.selectedDish.comments.map(c => (
+//                     <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
+//                 ))
+//             }
+//         </ListGroup>
+//         : <h1>NOTHING YET</h1>
+
+class DishComments extends React.Component {
+  render() {
+    if (this.props.selectedDish){
+        return (
+            <ListGroup>
+                <h2>Comments for {this.props.selectedDish.name}</h2>
+                {this.props.selectedDish.comments.map(c => (
                     <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
-                ))
-            }
-        </ListGroup>
-        : <h1>NOTHING YET</h1>
-
-
-export default DishComments
-
+                ))}
+            </ListGroup>
+        )
+  } else {
+      return (
+          <h1>NOTHING YET</h1>
+      )
+  }
+}
+}
+export default DishComments;
 // mapping props.selectedDish.comments
